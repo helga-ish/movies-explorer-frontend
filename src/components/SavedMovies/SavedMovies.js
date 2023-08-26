899ee1fb684ba3193632e90dc7b090b80e49573c
Helga Ish <belfegor.nomnom@gmail.com>
2023-08-26 22:05:46 +0800

    feat: create savedMovies

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -2,26 +2,49 @@ import React from "react";
 import MoviesCardList from "../MoviesCardList/MoviesCardList";
 import SearchForm from "../SearchForm/SearchForm";
 import './SavedMovies.css';
-import * as api from '../../utils/MoviesApi';
+import * as auth from '../../utils/MainApi';
 import Preloader from '../Preloader/Preloader';
 
 export default function SavedMovies({ loggedIn }) {
+    // empty array
+    const [isEmpty, setIsEmpty] = React.useState(false);
 
-        // loading
+    // short movies filter
+     const [isShortOff, setIsShortOff] = React.useState(false);
+
+    // emphasize saved movies for saved buttons
+        // const [isSaved, setIsSaved] = React.useState(false);
+
+    // remove
+        function handleRemoveMovie(movie) {
+            console.log(movie);
+            auth.deleteMovie(movie.movieId)
+            .then(() => {
+                setMovies((state) => state.filter(function(m) {
+                    return m.movieId !== movie.movieId;
+                }))
+                // setIsSaved(false);
+            })
+            .catch((error) => {
+                console.error(`Ошибка загрузки данных с сервера: ${error}`);
+            });
+        }
+
+    // loading
         const [isLoading, setIsLoading] = React.useState(false);
 
-        // error instead of data
+    // error instead of data
         const [isError, setIsError] = React.useState(false);
     
-        // getting movies
+     // getting movies
         const [movies, setMovies] = React.useState([]);
     
         React.useEffect(() => {
             setIsLoading(true);
-            api.getAllMovies()
-            .then((data) => {
+            auth.getSavedMovies()
+            .then((items) => {
                 setMovies(
-                    data.map((item) => ({
+                    items.data.map((item) => ({
                         owner: item.owner,
                         country: item.country,
                         director: item.director,
@@ -32,8 +55,8 @@ export default function SavedMovies({ loggedIn }) {
                         trailerLink: item.trailerLink,
                         nameRU: item.nameRU,
                         nameEN: item.nameEN,
-                        thumbnail: item.image,
-                        movieId: item.id,
+                        thumbnail: item.image.formats.thumbnail.url,
+                        movieId: item.movieId,
                     }))
                 )
             })
@@ -44,13 +67,17 @@ export default function SavedMovies({ loggedIn }) {
             .finally(() => {
                 setIsLoading(false);
             });
-        }, [loggedIn])
+        }, [])
 
         const findMovies = (filterParam) => {
             setIsLoading(true);
+            const filteredMovies = movies.filter((item) => item.nameRU.toLowerCase().includes(filterParam.toLowerCase()));
             setMovies(
-                movies.filter((item) => item.nameRU.toLowerCase().includes(filterParam.toLowerCase())).map((item) => ({ item }))
+                filteredMovies.map((item) => ( item ))
             );
+            if(filteredMovies.length === 0) {
+                setIsEmpty(true);
+            }
             setIsLoading(false);
         }
 
@@ -59,16 +86,23 @@ export default function SavedMovies({ loggedIn }) {
             <section className="saved-movies">
                 <SearchForm
                     findMovies = { findMovies }
+                    setIsShortOff = { setIsShortOff }
                 />
                 {isLoading ? (
                     <Preloader />
                     ) : (
                     isError ? (
-                        <h2 className="movies__error">Во время запроса произошла ошибка. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз</h2>
+                        <h2 className="movies__error">Во время запроса произошла ошибка. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз.</h2>
                     ) : (
-                        <MoviesCardList
-                            movies = { movies }
-                        />
+                        isEmpty ? (
+                            <h2 className="movies__error">Ничего не найдено.</h2>
+                            ) : (
+                            <MoviesCardList
+                                movies = { movies }
+                                isShortOff = { isShortOff }
+                                handleRemoveMovie = { handleRemoveMovie }
+                            />
+                            )
                         )
                     )
                 }
